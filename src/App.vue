42710c03e89505c1fc8caa78855da026bde47ef9
unjuanwebcat <jcardona@webcat.app>
2022-04-15 12:24:50 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Tareas/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tareas from './components/Tareas.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Tareas
   }
 }
 </script>
---
+++ src/components/Tareas.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+  <h1>Todo App</h1>
+  <label for="">Agregar tareas</label>
+  <input type="text" placeholder="Coloque aquí su nueva tarea" v-model="text"> <!-- /*Inyectamos variables de data() */ -->
+  <p>La nueva tarea es: {{ text }}</p> <!--/*Prueba de que la variable funciona*/ -->
+  <button @click="create">Agregar</button> <!--/*Invocamos métodos. Que están en "methods"  */ -->
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <p>{{todo.text}}</p>
+      <button @click="deleteTodo(todo)" style="background-color:red">Eliminar</button>
+    </li>
+  </ul>
+</div>
+</template>
+
+
+
+<script>
+export default
+{
+  data() /*Variables que ingresa usuario*/
+  {
+    return{
+      id:1,
+      todos:[],
+      text:''
+    }
+  },
+  methods:{
+    restart()
+    {
+      this.id++
+      this.text=''
+    },
+
+    create()
+    {
+      const todo = {
+        id:this.id,
+        text:this.text
+      }
+      if(this.text!='')
+      {
+        this.todos.push(todo)
+      }
+      this.restart()
+    },
+    deleteTodo(todo)
+    {
+      const idx = this.todos.indexOf(todo)
+      this.todos.splice(idx,1)
+    }
+  }
+}
+</script>
